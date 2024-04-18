cf7aa537bc30a07c269331048774fc62c82b31f5
Itxaka <itxaka@kairos.io>
2024-04-18 10:51:08 +0200

    First commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module keydump
+
+go 1.22.1
+
+require (
+	github.com/foxboron/go-uefi v0.0.0-20240128152106-48be911532c2 // indirect
+	github.com/kairos-io/kairos-sdk v0.1.2-0.20240418084240-21cf40ea2b06 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/zerolog v1.32.0 // indirect
+	github.com/spf13/afero v1.9.3 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kairos-io/kairos-sdk/signatures"
+	"os"
+	"strings"
+)
+
+func main() {
+	certs, err := signatures.GetAllFullCerts()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for index, cert := range certs.PK {
+		commonName := strings.Replace(cert.Subject.CommonName, " ", "_", -1)
+		filename := fmt.Sprintf("PK-%s-%d.pem", commonName, index)
+		err := os.WriteFile(filename, cert.Raw, 755)
+		if err != nil {
+			fmt.Printf("Could not write file at %s: %s\n", filename, err)
+			continue
+		}
+		fmt.Printf("Wrote file at %s\n", filename)
+	}
+
+	for index, cert := range certs.KEK {
+		commonName := strings.Replace(cert.Subject.CommonName, " ", "_", -1)
+		filename := fmt.Sprintf("KEK-%s-%d.pem", commonName, index)
+		err := os.WriteFile(filename, cert.Raw, 755)
+		if err != nil {
+			fmt.Printf("Could not write file at %s: %s\n", filename, err)
+			continue
+		}
+		fmt.Printf("Wrote file at %s\n", filename)
+	}
+
+	for index, cert := range certs.DB {
+		commonName := strings.Replace(cert.Subject.CommonName, " ", "_", -1)
+		filename := fmt.Sprintf("DB-%s-%d.pem", commonName, index)
+		err := os.WriteFile(filename, cert.Raw, 755)
+		if err != nil {
+			fmt.Printf("Could not write file at %s: %s\n", filename, err)
+			continue
+		}
+		fmt.Printf("Wrote file at %s\n", filename)
+	}
+}
